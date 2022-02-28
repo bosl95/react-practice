fafc19d2b8e3bf2734cc47302c7ea8995c93b6c6
bosl95 <bosl95@naver.com>
2022-02-28 11:48:54 +0900

    feat: useState 변경 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['글 리스트1', '글 리스트2', '글 리스트3']);
+  let [따봉, 따봉변경] = useState(0);
+
   let posts = "강남 고기 맛집"
   return (
     <div className="App">
@@ -10,7 +15,17 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3>{ posts }</h3>  
+        <h3>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }> 👍 </span> {따봉} </h3>  
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[1] }</h3>  
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[2] }</h3>  
         <p>2월 17일 발행</p>
         <hr/>
       </div>
