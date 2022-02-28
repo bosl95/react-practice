852ec20ed166c6754df44eed655dbd9f86869db8
bosl95 <bosl95@naver.com>
2022-02-28 12:18:46 +0900

    feat: 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState(['글 리스트1', '글 리스트2', '글 리스트3']);
+  let [글제목, 글제목변경] = useState(['다) 글 리스트1', '가) 글 리스트2', '나) 글 리스트3']);
   let [따봉, 따봉변경] = useState(0);
-
+ 
   // let posts = "강남 고기 맛집"
 
   function 제목바꾸기() {
@@ -39,8 +39,20 @@ function App() {
         <p>2월 17일 발행</p>
         <hr/>
       </div>
+
+      <Modal />
     </div>
   );
+}
+
+function Modal() { // Modal 이라는 컴포넌트 생성
+  return (
+    <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
