487c2dd7936cb34e55089a5f923b715fa2bf644b
bosl95 <bosl95@naver.com>
2022-02-28 12:00:24 +0900

    feat: state 값 변경

--- src/App.js
+++ src/App.js
@@ -8,12 +8,22 @@ function App() {
   let [글제목, 글제목변경] = useState(['글 리스트1', '글 리스트2', '글 리스트3']);
   let [따봉, 따봉변경] = useState(0);
 
-  let posts = "강남 고기 맛집"
+  // let posts = "강남 고기 맛집"
+
+  function 제목바꾸기() {
+    // 글제목변경(['글 리스트1 변경', '글 리스트2 변경', '글 리스트3 변경']);
+
+    var newArray = [...글제목]; // deep copy
+    newArray[0] = '글 리스트1만 변경'
+    글제목변경( newArray );
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={ 제목바꾸기 }> 글제목 변경 </button>
       <div className="list">
         <h3>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }> 👍 </span> {따봉} </h3>  
         <p>2월 17일 발행</p>
